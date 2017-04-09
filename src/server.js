1d0407fe84335ffaa5b759f7ec7009080b8310f1
Jack Marchant <jack@expert360.com>
2017-04-09 12:23:30 +1000

    Restructure files

--- src/server.js
+++ src/server.js
@@ -1,18 +1,15 @@
 require('babel-register')({
-    presets: ['react', 'es2015']
+  presets: ['react', 'es2015']
 });
 
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var createRoutes = require('./routes');
 
 app.use(express.static('public'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-var createRoutes = require('./routes');
-
 createRoutes(app);
 
-var PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(process.env.PORT || 3000);
