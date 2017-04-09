d31a74dcb6b17297616a75d7ca5dacdc5846cd10
Jack Marchant <jack@expert360.com>
2017-04-09 12:33:39 +1000

    Fix render error

--- src/getComponent.js
+++ src/getComponent.js
@@ -1,7 +1,7 @@
 var React = require('react');
 
 // internal components
-var Component = require('./Component');
+var Component = require('./components/Component');
 
 var getComponents = function(component, props) {
   var internalComponents = {
